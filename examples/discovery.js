9735ceaa1bf3a13a0bff0e2e3965ef62169c5592
Tomas Aparicio <tomas@aparicio.me>
2014-10-07 01:27:50 +0200

    feat(example): add

---
+++ examples/discovery.js
@@ -0,0 +1,22 @@
+var Resilient = require('../')
+
+var client = Resilient({
+  service: {
+    timeout: 1000
+  },
+  discovery: {
+    servers: [
+      'http://localhost:8882/unavailable',
+      'http://localhost:8882/timeout',
+      'http://localhost:8882/valid'
+    ],
+    timeout: 1000,
+    parallel: false
+  }
+})
+
+client('/hello', function (err, res) {
+  console.log('Error:', err)
+  console.log('Response:', res)
+  console.log('Body:', res.data)
+})
---
+++ examples/servers.js
@@ -0,0 +1,18 @@
+var Resilient = require('../')
+
+var client = Resilient({
+  service: {
+    servers: [
+      'http://localhost:8882/unavailable',
+      'http://localhost:8882/timeout',
+      'http://localhost:8882/server'
+    ],
+    timeout: 1000
+  }
+})
+
+http('/hello', function (err, res) {
+  console.log('Error:', err)
+  console.log('Response:', res)
+  console.log('Body:', res.data)
+})
