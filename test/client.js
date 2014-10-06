bf7e731d7046ce3f0ed5cb2892fc63b5b48d948b
Tomas Aparicio <tomas@aparicio.me>
2014-10-06 08:25:42 +0200

    feat(test): add coverage for client

---
+++ test/client.js
@@ -0,0 +1,159 @@
+var expect = require('chai').expect
+var nock = require('nock')
+var Resilient = require('../')
+
+describe('Client', function () {
+  var client = Resilient({
+    discovery: {
+      timeout: 50,
+      servers: [
+        'http://timeout/1',
+        'http://unavailable',
+        'http://timeout/2',
+        'http://valid'
+      ]
+    }
+  }).client()
+
+  before(function () {
+    nock('http://unnavailable')
+      .filteringPath(/\?(.*)/g, '')
+      .persist()
+      .get('/')
+      .reply(503)
+    nock('http://timeout')
+      .filteringPath(function () { return '/' })
+      .persist()
+      .get('/')
+      .delayConnection(100)
+      .reply(500)
+    nock('http://valid')
+      .filteringPath(/\?(.*)/g, '')
+      .persist()
+      .get('/')
+      .reply(200, [ 'http://server' ])
+  })
+
+  after(function () {
+    nock.cleanAll()
+  })
+
+  describe('send', function () {
+    before(function () {
+      nock('http://server')
+        .post('/hello')
+        .reply(200, { hello: 'world' })
+    })
+
+    it('should perform a valid request', function (done) {
+      client.send('/hello', { method: 'POST' }, function (err, res) {
+        expect(err).to.be.null
+        expect(res.status).to.be.equal(200)
+        expect(res.data).to.be.deep.equal({ hello: 'world' })
+        done()
+      })
+    })
+  })
+
+  describe('GET', function () {
+    before(function () {
+      nock('http://server')
+        .get('/hello')
+        .reply(200, { hello: 'world' })
+    })
+
+    it('should perform a valid request', function (done) {
+      client.get('/hello', function (err, res) {
+        expect(err).to.be.null
+        expect(res.status).to.be.equal(200)
+        expect(res.data).to.be.deep.equal({ hello: 'world' })
+        done()
+      })
+    })
+  })
+
+  describe('POST', function () {
+    before(function () {
+      nock('http://server')
+        .post('/hello')
+        .reply(200, { hello: 'world' })
+    })
+
+    it('should perform a valid request', function (done) {
+      client.post('/hello', function (err, res) {
+        expect(err).to.be.null
+        expect(res.status).to.be.equal(200)
+        expect(res.data).to.be.deep.equal({ hello: 'world' })
+        done()
+      })
+    })
+  })
+
+  describe('PUT', function () {
+    before(function () {
+      nock('http://server')
+        .put('/hello')
+        .reply(200, { hello: 'world' })
+    })
+
+    it('should perform a valid request', function (done) {
+      client.put('/hello', function (err, res) {
+        expect(err).to.be.null
+        expect(res.status).to.be.equal(200)
+        expect(res.data).to.be.deep.equal({ hello: 'world' })
+        done()
+      })
+    })
+  })
+
+  describe('DELETE', function () {
+    before(function () {
+      nock('http://server')
+        .delete('/hello')
+        .reply(200, { hello: 'world' })
+    })
+
+    it('should perform a valid request', function (done) {
+      client.del('/hello', function (err, res) {
+        expect(err).to.be.null
+        expect(res.status).to.be.equal(200)
+        expect(res.data).to.be.deep.equal({ hello: 'world' })
+        done()
+      })
+    })
+  })
+
+  describe('PATCH', function () {
+    before(function () {
+      nock('http://server')
+        .patch('/hello')
+        .reply(200, { hello: 'world' })
+    })
+
+    it('should perform a valid request', function (done) {
+      client.patch('/hello', function (err, res) {
+        expect(err).to.be.null
+        expect(res.status).to.be.equal(200)
+        expect(res.data).to.be.deep.equal({ hello: 'world' })
+        done()
+      })
+    })
+  })
+
+  describe('HEAD', function () {
+    before(function () {
+      nock('http://server')
+        .head('/hello')
+        .reply(200, { hello: 'world' })
+    })
+
+    it('should perform a valid request', function (done) {
+      client.head('/hello', function (err, res) {
+        expect(err).to.be.null
+        expect(res.status).to.be.equal(200)
+        expect(res.data).to.be.deep.equal({ hello: 'world' })
+        done()
+      })
+    })
+  })
+})
