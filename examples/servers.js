55a3ec9aea523b5590588afa1badb514253b9907
Tomas Aparicio <tomas@aparicio.me>
2014-10-26 12:41:32 +0100

    chore(example): rename

--- examples/servers.js
+++ examples/servers.js
@@ -7,14 +7,7 @@ var client = Resilient({
       'http://localhost:8882/timeout',
       'http://localhost:8882/server'
     ],
-    timeout: 1000,
-    basePath: '/api',
-    method: 'POST',
-    retry: 3,
-    retryWait: 500,
-    headers: {
-      Authorization: 'Bearer 0b79bab50daca910b000d4f1a2b675d604257e42'
-    }
+    timeout: 1000
   }
 })
 
