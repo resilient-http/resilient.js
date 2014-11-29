59080843243b1f6fabc6b3a77431f13664446a2a
Tomas Aparicio <tomas@aparicio.me>
2014-11-29 20:34:02 +0100

    chore(version): bump

--- test/browser.js
+++ test/browser.js
@@ -63,6 +63,50 @@ describe('Browser', function () {
     })
   })
 
+  describe('service', function () {
+    var client = Resilient({
+      service: {
+        timeout: 250,
+        retry: 0,
+        servers: [
+          'http://localhost:8882/server/unavailable',
+          'http://localhost:8882/server/timeout',
+          'http://localhost:8882/server/fail',
+          'http://localhost:8882/server/delay',
+          'http://localhost:8882/server/1'
+        ]
+      }
+    })
+
+    it('should perform a GET request', function (done) {
+      client.get('/', { timeout: 250 }, function (err, res) {
+        expect(res.status).to.be.equal(200)
+        done()
+      })
+    })
+
+    it('should perform a POST request', function (done) {
+      client.post('/', { timeout: 250, data: { hello: 'world' } }, function (err, res) {
+        expect(res.status).to.be.equal(200)
+        done()
+      })
+    })
+
+    it('should perform a PUT request', function (done) {
+      client.put('/', { timeout: 250, data: { hello: 'world' } }, function (err, res) {
+        expect(res.status).to.be.equal(200)
+        done()
+      })
+    })
+
+    it('should perform a DELETE request', function (done) {
+      client.del('/', { timeout: 250, data: { hello: 'world' } }, function (err, res) {
+        expect(res.status).to.be.equal(200)
+        done()
+      })
+    })
+  })
+
   describe('proxy HTTP client', function () {
     var client = Resilient({
       discovery: {
