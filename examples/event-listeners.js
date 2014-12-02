e9cc0559239505262690c8a2d4a3edbda28609ad
Tomas Aparicio <tomas@aparicio.me>
2014-12-02 18:18:41 +0100

    feat(examples): add interceptor example

---
+++ examples/event-listeners.js
@@ -0,0 +1,30 @@
+var Resilient = require('../')
+
+var client = Resilient({
+  discovery: {
+    retry: 1,
+    servers: [
+      'http://localhost:8882/discovery/unavailable',
+      'http://localhost:8882/discovery/timeout',
+      'http://localhost:8882/discovery/unavailable'
+    ]
+  }
+})
+
+client.on('request:retry', function (options, servers) {
+  console.log('Retry request cycle was dispached!')
+  console.log('Using the following options:', options)
+  console.log('Using the following servers:', servers)
+})
+
+client.on('servers:refresh', function (servers) {
+  console.log('New servers list:', servers.join(', '))
+})
+
+client.on('servers:cache', function (servers) {
+  console.log('Cached servers list:', servers.join(', '))
+})
+
+client.on('discovery:refresh', function (servers) {
+  console.log('New up-to-date server list:', servers.join(', '))
+})
---
+++ examples/interceptors.js
@@ -0,0 +1,33 @@
+var Resilient = require('../')
+
+var client = Resilient({
+  discovery: {
+    retry: 1,
+    servers: [
+      'http://localhost:8882/discovery/unavailable',
+      'http://localhost:8882/discovery/timeout',
+      'http://localhost:8882/discovery/unavailable'
+    ]
+  }
+})
+
+// dispatch before send the request
+client.on('request:start', function (options) {
+  // add a random request identifier
+  options.headers['X-Request-Id'] = '37779240-7a46-11e4-82f8-0800200c9a66'
+  // and the app client version number
+  options.headers['X-Client-Version'] = '1.0.5'
+})
+
+// dispatch after any request was finished, with success or error status
+client.on('request:finish', function (err, res) {
+  if (err) {
+    // do something when error happends, like logging the error
+    console.error('The request was failed:', err)
+  } else if (res.status >= 400) {
+    // when an unexpected server response happends, do something...
+    console.error('Invalid server response:', res.status)
+  } else {
+    console.log('Request success:', res.config.url)
+  }
+})
