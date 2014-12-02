718d06ca6aec43ff92c7cf3539ef0232d73d86ad
Tomas Aparicio <tomas@aparicio.me>
2014-12-02 19:51:56 +0100

    chore(test): add SauceLabs karma plugin

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,30 @@
 module.exports = function(config) {
+  var customLaunchers = {
+    sl_chrome: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows 7',
+      version: '35'
+    },
+    sl_firefox: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '30'
+    },
+    sl_ios_safari: {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      platform: 'OS X 10.9',
+      version: '7.1'
+    },
+    sl_ie_11: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    }
+  }
+
   config.set({
     files: [
       'node_modules/chai/chai.js',
@@ -7,6 +33,10 @@ module.exports = function(config) {
       'test/browser.js'
     ],
     frameworks: ['mocha'],
+    sauceLabs: {
+      testName: 'Web App Unit Tests'
+    },
+    customLaunchers: customLaunchers,
     browsers: [
       'Chrome',
       'ChromeCanary',
@@ -14,8 +44,8 @@ module.exports = function(config) {
       'PhantomJS',
       'Safari',
       'Opera'
-    ],
-    reports: ['progress'],
+    ], // Object.keys(customLaunchers)
+    reports: ['progress', 'saucelabs'],
     singleRun: true
   })
 }
