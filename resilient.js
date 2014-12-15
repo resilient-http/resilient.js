50aea43fba1441e79f530941fca35f6a04048441
Tomas Aparicio <tomas@aparicio.me>
2014-12-15 19:19:53 +0100

    refactor(utils): remove exports alias

--- resilient.js
+++ resilient.js
@@ -356,12 +356,6 @@ function Client(resilient) {
   this._resilient = resilient
 }
 
-Client.prototype.send = function (path, options, cb, method) {
-  var args = normalizeArgs.call(this, path, options, cb, method)
-  requester.apply(this, args)
-  return this
-}
-
 Client.prototype.get = function (path, options, cb) {
   return this.send(path, options, cb, 'GET')
 }
@@ -374,7 +368,8 @@ Client.prototype.put = function (path, options, cb) {
   return this.send(path, options, cb, 'PUT')
 }
 
-Client.prototype.delete = Client.prototype.del = function (path, options, cb) {
+Client.prototype.del =
+Client.prototype.delete = function (path, options, cb) {
   return this.send(path, options, cb, 'DELETE')
 }
 
@@ -384,6 +379,12 @@ Client.prototype.patch = function (path, options, cb) {
 
 Client.prototype.head = function (path, options, cb) {
   return this.send(path, options, cb, 'HEAD')
+}
+
+Client.prototype.send = function (path, options, cb, method) {
+  var args = normalizeArgs.call(this, path, options, cb, method)
+  requester.apply(this, args)
+  return this
 }
 
 function requester(options, cb) {
@@ -862,8 +863,8 @@ Options.prototype.clone = function () {
 
 Options.define = function (options, defaultOptions) {
   var store = new Options()
-  defaultOptions = _.clone(defaultOptions || defaults)
-  Object.keys(defaultOptions).forEach(defineDefaults(options, store))
+  Object.keys( _.clone(defaultOptions || defaults))
+    .forEach(defineDefaults(options, store))
   return store
 }
 
@@ -1834,31 +1835,31 @@ var bind = Function.prototype.bind
 var isArrayNative = Array.isArray
 var uriRegex = /^http[s]?\:\/\/(.+)/i
 
-_.noop = function noop() {}
+exports.noop = function noop() {}
 
-_.now = function () {
+exports.now = function () {
   return new Date().getTime()
 }
 
-_.emptyObject = function () {
+exports.emptyObject = function () {
   return Object.create(null)
 }
 
-_.isObj = function (o) {
+exports.isObj = function (o) {
   return o && toStr.call(o) === '[object Object]' || false
 }
 
-_.isArr = function (o) {
+exports.isArr = function (o) {
   return o && isArrayNative ? isArrayNative(o) : toStr.call(o) === '[object Array]' || false
 }
 
-_.bind = function (ctx, fn) {
+exports.bind = function (ctx, fn) {
   return bind ? fn.bind(ctx) : function () {
     return fn.apply(ctx, arguments)
   }
 }
 
-_.each = function (obj, fn) {
+exports.each = function (obj, fn) {
   var i, l
   if (_.isArr(obj))
     for (i = 0, l = obj.length; i < l; i += 1) fn(obj[i], i)
@@ -1866,11 +1867,11 @@ _.each = function (obj, fn) {
     for (i in obj) if (hasOwn.call(obj, i)) fn(i, obj[i])
 }
 
-_.clone = function (obj) {
+exports.clone = function (obj) {
   return _.extend({}, obj)
 }
 
-_.omit = function (obj, keys) {
+exports.omit = function (obj, keys) {
   var key, buf = {}
   if (_.isObj(obj)) {
     for (key in obj) if (hasOwn.call(obj, key)) {
@@ -1880,23 +1881,23 @@ _.omit = function (obj, keys) {
   return buf
 }
 
-_.delay = function (fn, ms) {
+exports.delay = function (fn, ms) {
   return setTimeout(fn, ms || 1)
 }
 
-_.isURI = function (str) {
+exports.isURI = function (str) {
   return typeof str === 'string' && uriRegex.test(str)
 }
 
-_.join = function (base) {
+exports.join = function (base) {
   return (base || '') + (slice.call(arguments, 1)
     .filter(function (part) { return typeof part === 'string' && part.length > 0 })
     .join(''))
 }
 
-_.extend = objIterator(extender)
+exports.extend = objIterator(extender)
 
-_.merge = objIterator(merger)
+exports.merge = objIterator(merger)
 
 function extender(target, key, value) {
   target[key] = value
